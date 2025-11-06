b6c30f65d63bb40f485a3c73d5112fe480b53351
Bryan M <106822537+BiMq0@users.noreply.github.com>
2025-11-06 16:20:43 -0400

    Implement InventoryScript for item management, alv la ia de copilot para commits esta goood

---
+++ Assets/Scripts/Globales/InventoryScript.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour
+{
+    private Dictionary<string, GameObject> itemsEnInventario = new Dictionary<string, GameObject>();
+
+    private static InventoryScript instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject); 
+    }
+
+    public static InventoryScript Instance => instance;
+
+    public void AgregarItem(string nombreItem, GameObject item)
+    {
+        if (!itemsEnInventario.ContainsKey(nombreItem))
+        {
+            itemsEnInventario.Add(nombreItem, item);
+            Debug.Log($"Item agregado: {nombreItem}");
+        }
+        else
+        {
+            Debug.Log($"El item '{nombreItem}' ya está en el inventario, esta vaina no deberia pasar porque los items desaparecen xd");
+        }
+    }
+
+    public void EliminarItem(string nombreItem)
+    {
+        if (itemsEnInventario.ContainsKey(nombreItem))
+        {
+            itemsEnInventario.Remove(nombreItem);
+        }
+        else
+        {
+            Debug.Log($"No se encontró el item '{nombreItem}' en el inventario, debe estar en otra escena xd");
+        }
+    }
+
+    public bool TieneItem(string nombreItem)
+    {
+        return itemsEnInventario.ContainsKey(nombreItem);
+    }
+
+    public void VaciarInventario()
+    {
+        itemsEnInventario.Clear();
+    }
+
+    public void MostrarInventario()
+    {
+        foreach (var kvp in itemsEnInventario)
+        {
+            Debug.Log($"• {kvp.Key}");
+        }
+    }
+}
